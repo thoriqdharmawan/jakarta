2c4efd8847e11f6faf51510bcdd4ccad95bdf63d
Thoriq Dharmawan <skyshi-thoriq.darmawan@edot.id>
2025-01-18 08:34:50 +0700

    feat: added VCalendar and its CSS to main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VCalendar from 'v-calendar'
+import 'v-calendar/style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VCalendar, {})
+app.mount('#app')

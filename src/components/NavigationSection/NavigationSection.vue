497e4254555affa4b356e3a1483e06f6e285d2de
Thoriq Dharmawan <skyshi-thoriq.darmawan@edot.id>
2025-01-18 18:24:00 +0700

    feat: Update NavigationSection.vue with accessibility improvements and content changes

--- src/components/NavigationSection/NavigationSection.vue
+++ src/components/NavigationSection/NavigationSection.vue
@@ -1,40 +1,34 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" role="navigation" aria-label="Main navigation">
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="/" title="Kunjungi Jakarta Official Website"
+        aria-label="Home page of Jakarta">Jakarta</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#" title="Pelajari lebih lanjut tentang Jakarta"
+              aria-label="Mengenal Jakarta">Mengenal Jakarta</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <a class="nav-link" href="#" title="Informasi layanan yang tersedia di Jakarta"
+              aria-label="Informasi Layanan">Informasi Layanan</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <a class="nav-link" href="#" title="Jelajahi tempat-tempat menarik di Jakarta"
+              aria-label="Jelajah Jakarta">Jelajah Jakarta</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" title="Lihat informasi terkait PPID Jakarta" aria-label="PPID Jakarta">PPID</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" title="Pusat media informasi resmi Jakarta" aria-label="Pusat Media">Pusat
+              Media</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-dark" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
